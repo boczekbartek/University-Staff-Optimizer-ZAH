99589b75d56b3f22b39597b3a8e901726f8a278f
piotrowskialek <piotrowskialek@icloud.com>
2018-05-05 12:32:25 +0200

    posprzatanie balaganu 4

---
+++ input_data/__init__.py
@@ -0,0 +1,3 @@
+from .Constants import Przedmioty
+from .Constants import max_budget
+from .Constants import max_working_hours
--- main.py
+++ main.py
@@ -1,24 +1,7 @@
 from dto import *
-from model import Solution
+from model import *
 from enumy import *
-
-# wartosci wejsciowe
-max_working_hours = 140  # maksymalna liczba godzin dla pracownika
-max_budget = 100000  # maksymalny budzet uczelni
-
-Przedmioty = [
-    Przedmiot(nazwa="POBO", dziedzina=Dziedzina.POL, typ=TypJednostkiDyd.LAB, liczba_godzin=4),
-    Przedmiot(nazwa="POBO", dziedzina=Dziedzina.POL, typ=TypJednostkiDyd.WYKLAD, liczba_godzin=2),
-    Przedmiot(nazwa="SKM", dziedzina=Dziedzina.CHEM, typ=TypJednostkiDyd.LAB, liczba_godzin=4),
-    Przedmiot(nazwa="ZAH", dziedzina=Dziedzina.CHEM, typ=TypJednostkiDyd.WYKLAD, liczba_godzin=3),
-    Przedmiot(nazwa="ZAH", dziedzina=Dziedzina.CHEM, typ=TypJednostkiDyd.LAB, liczba_godzin=5),
-    Przedmiot(nazwa="SNR", dziedzina=Dziedzina.CHEM, typ=TypJednostkiDyd.LAB, liczba_godzin=2),
-    Przedmiot(nazwa="SNR", dziedzina=Dziedzina.CHEM, typ=TypJednostkiDyd.WYKLAD, liczba_godzin=4),
-    Przedmiot(nazwa="ZPI", dziedzina=Dziedzina.FIZ, typ=TypJednostkiDyd.LAB, liczba_godzin=3),
-    Przedmiot(nazwa="ZPI", dziedzina=Dziedzina.FIZ, typ=TypJednostkiDyd.WYKLAD, liczba_godzin=2)
-]
-
-# wartosci wyjsciowe
+from input_data import *
 
 
 if __name__ == '__main__':
