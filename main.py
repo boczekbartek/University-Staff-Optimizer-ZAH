399ab2edac64f8f6567e175b97b20e610ff9ca19
piotrowskialek <piotrowskialek@icloud.com>
2018-05-05 12:35:42 +0200

    posprzatanie balaganu 5

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ from input_data import *
 
 
 if __name__ == '__main__':
-    a = Solution(2, 2, 1, 0.1)
+    a = Solution(alfa=2, beta=2, wsp_pasujacej_jakosci=1, wsp_niepasujacej_jakosci=0.1)
     print(a.oblicz_jakosc_pracownika(Pracownik(StanowiskoPracownika.PROFESOR, Dziedzina.POL), Przedmioty[0]))
