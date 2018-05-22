955647af2445424b484260d37c2799038f81cc7d
Bartłomiej Boczek <goterix1@gmail.com>
2018-05-23 00:57:24 +0200

    Optimization of matrix (with flattening)

---
+++ model/optimize2.py
@@ -0,0 +1,23 @@
+from scipy.optimize import minimize, rosen, rosen_der
+import numpy as np
+from functools import partial
+
+def fun_to_optimize(matrix):
+    res = 0
+    for row in matrix:
+        res += rosen(row)
+    return res / matrix.shape[0]
+
+
+def de_flatten(vec, columns):
+    return fun_to_optimize(vec.reshape(-1, columns))
+
+
+x0 = np.array([[1.3, 0.7, 0.8, 1.9, 1.2],
+               [1.3, 0.7, 0.8, 1.9, 1.2],
+               [1.3, 0.7, 0.8, 1.9, 1.2]])
+
+columns = 5
+opt_fun = partial(de_flatten, columns=columns)
+res = minimize(opt_fun, x0.flatten(), method='Nelder-Mead', tol=1e-6)
+print(res.x.reshape(-1, columns))
